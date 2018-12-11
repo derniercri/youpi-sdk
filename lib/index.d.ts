af24b777b19d2c448cd22fa0e020cc846c54c35a
Guillaume Bailleul <laibulle@gmail.com>
2018-12-11 23:46:25 +0100

    chore: add compiled files

---
+++ lib/index.d.ts
@@ -0,0 +1,46 @@
+import * as moment from 'moment';
+import { Sensor, SensorPreferences, User, Group, Issue, EndpointIssue, StatusCheck, CommentCategory, DeviceToken, SeriePoint, RegisterForm } from './models';
+export * from './models';
+export declare type TokenRes = {
+    token?: string;
+    message?: string;
+};
+export declare const filterUp: (items: Sensor[]) => Sensor[];
+export declare const filterDown: (items: Sensor[]) => Sensor[];
+export default class Sdk {
+    url: string;
+    token: string;
+    private tokenExpiredCb;
+    constructor(url: string);
+    onTokenExpired(cb: () => void): void;
+    resHandler(res: Response): any;
+    setToken: (token: string) => void;
+    login: (user: string, password: string) => Promise<TokenRes>;
+    createHeaders: (token: string) => {
+        'Authorization': string;
+        'Content-Type': string;
+    };
+    createPreferences: (preferences: SensorPreferences) => Promise<Response>;
+    updatePreSensorferences: (preferences: SensorPreferences) => Promise<Response>;
+    groups: () => Promise<Group[]>;
+    updateUser: (user: User) => Promise<User>;
+    getUser: (userId: string) => Promise<User>;
+    comments: (issueId: number) => Promise<Comment[]>;
+    sensors: (groupId: number) => Promise<Sensor[]>;
+    sensorCheck: (id: number) => Promise<StatusCheck>;
+    issues: (groupId: number, stat: string) => Promise<Issue[]>;
+    updateIssue: (issue: Issue) => Promise<Response>;
+    updateEndpointIssue: (endpointIssue: EndpointIssue) => Promise<Response>;
+    sensorsPreferences: () => Promise<SensorPreferences[]>;
+    commentCategories: () => Promise<CommentCategory[]>;
+    users: () => Promise<User[]>;
+    owners: (userId: number) => Promise<User[]>;
+    createDeviceToken: (device: string, type: "IOS" | "ANDROID") => Promise<DeviceToken>;
+    updatePreferences: (preferences: SensorPreferences) => Promise<Response>;
+    getSeries: (id: number, start: moment.Moment, stop: moment.Moment) => Promise<SeriePoint[]>;
+    register: (userForm: RegisterForm) => Promise<User>;
+    createOwner: (sensorId: number, userId: number) => Promise<User>;
+    createComment: (issueId: number, categoryId: number, content: string) => Promise<any>;
+}
+export declare const initSdk: (url: string) => Sdk;
+export declare const getSdk: () => Sdk;
---
+++ lib/models.d.ts
@@ -0,0 +1,92 @@
+export declare type Token = string;
+export declare const DOMAIN = 0;
+export declare const APP = 1;
+export declare const SENSOR = 2;
+export declare type Status = 'OK' | 'KO' | 'HANDLED';
+export declare type EndpointIssue = {
+    id: number;
+    sensorId: number;
+    responsibleId: number | null;
+};
+export declare type Group = {
+    id: number;
+    name: string;
+    sensors: number[];
+};
+export declare type Statistics = {
+    day: number;
+    week: number;
+    month: number;
+    latency: number;
+};
+export declare type SensorType = 0 | 1 | 2;
+export declare type Sensor = {
+    id: number;
+    status: Status;
+    type: SensorType;
+    label: string;
+    statistics: Statistics;
+    parentId: number;
+    statusUpdatedAt: string;
+};
+export declare type SeriePoint = {
+    time: number;
+    latency: number;
+    status: Status;
+};
+export declare type Issue = {
+    id: number;
+    status: 'OPEN' | 'CLOSED';
+    createdAt: string;
+    resolvedAt: string;
+    endpoints: number[];
+    sensorId: number;
+    endpointsInfo: EndpointIssue[];
+};
+export declare type StatusCheck = {
+    statusCode: string;
+    body: string;
+};
+export declare type DeviceToken = {
+    id: number;
+    token: string;
+    type: 'IOS' | 'ANDROID';
+};
+export declare type CommentCategory = {
+    id: number;
+    label: string;
+};
+export declare type WeatherType = 0 | 1 | 2;
+export declare type SensorPreferences = {
+    id: number;
+    favorite: 0 | 1 | 2;
+    notifications: 0 | 1 | 2;
+    sensorId: number;
+    locked: boolean;
+    follow: 0 | 1;
+};
+export declare type User = {
+    id: number;
+    lastName: string;
+    firstName: string;
+    email: string;
+    phone: string;
+    silent: boolean;
+};
+export declare type RegisterForm = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+};
+export declare type Comment = {
+    id: number;
+    userId: number;
+    issueId: number;
+    content: string;
+    categoryId: number;
+    createdAt: string;
+};
+export declare type MeteoSubType = 5 | 6 | 7;
+export declare type MeteoType = SensorType | MeteoSubType;
+export declare const dataTypeText: (t?: number | undefined) => "" | "domaine" | "application" | "sonde" | "accueil" | "listes" | "incidents";
